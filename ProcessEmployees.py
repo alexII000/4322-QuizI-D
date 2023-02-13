d84b21309b0cd665164dfa7f988a4d64dabc2ca3
AlexII000 <alex_chon1@baylor.edu>
2023-02-13 15:20:18 -0600

    s

--- ProcessEmployees.py
+++ ProcessEmployees.py
@@ -1,4 +1,4 @@
-'''
+"""
 Due the outstanding performance of the Customer Service Represetatives (CSRs) in the marketing department, management
 has decided to reward them with a 10% increase in their salary. To evaluate the impact on the budget, you have been
 asked to run a report on the employee file and display the results of BEFORE AND AFTER the salary increase.
@@ -6,36 +6,44 @@ asked to run a report on the employee file and display the results of BEFORE AND
 You will create a dictionary that has the full employee name as the key and ONLY their new salary as the value.
 
 Iternate through the dictionary to print out their name and thier new salary (as in image)
-'''
+"""
 
 import csv
 
-#open the file
+# open the file
 
 
+infile = open("employee_data.csv", "r")
+sales = csv.reader(infile, delimiter=",")
+next(sales)
 
+# create an empty dictionary
+csr_dict = {}
 
-#create an empty dictionary
+# use a loop to iterate through the csv file
+for row in sales:
+    first_name = row[1]
+    last_name = row[2]
+    old_salary = row[5]
+    salary = float(row[5]) * 1.1
 
+    full_name = first_name + " " + last_name
 
-#use a loop to iterate through the csv file
+    csr_dict[full_name] = salary
 
+    # check if the employee fits the search criteria
 
-    #check if the employee fits the search criteria
-
-
-    
+    print(f"Manager Name: {full_name} Current Salary: ${old_salary}")
 
 print()
-print('=========================================')
+print("=========================================")
 print()
 
-#iternate through the dictionary and print out the key and value as per printout
+for row in sales:
+    infile = open("employee_data.csv", "r")
+sales = csv.reader(infile, delimiter=",")
+next(sales)
+    print(f"Manager Name: {full_name} New Salary: ${salary}")
+# iternate through the dictionary and print out the key and value as per printout
 
-
-
-          
-        
-
-        
-    
+print(f"")
